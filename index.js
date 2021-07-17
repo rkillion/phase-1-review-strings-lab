0191dcfd0fc5d3230cf5f53cdb919572f98486e6
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-07-17 12:38:37 -0400

    commit tree 818e2dfd932834967c380f7e4ac8e75988130b84

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Grace Hopper";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
